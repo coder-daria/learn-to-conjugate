7601845e5d9c0a1ebe6e6507d2683294c98dcdeb
Christian de la Hoz <apigolas@gmail.com>
2020-05-01 09:31:30 +0200

    Close settings popover when user clicks outside

--- src/components/Popover/Popover.js
+++ src/components/Popover/Popover.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+
+import { useClickOutside } from '../../utils/useClickOutside';
 
 import { Container, DropDown } from './Popover.styles';
 
-const Popover = ({ icon, children, onClick, forwardRef }) => {
-  const [isOpen, togglePopover] = useState(false);
+const Popover = ({ icon, children, onClick }) => {
+  const ref = useRef();
+
+  const [isOpen, togglePopover] = useState();
 
   const handleClick = () => {
     if(onClick) {
@@ -13,8 +17,14 @@ const Popover = ({ icon, children, onClick, forwardRef }) => {
     togglePopover(!isOpen);
   };
 
+  useClickOutside((ref), () => {
+    if(ref) {
+      togglePopover();
+    }
+  });
+
   return (
-    <Container onClick={handleClick}  ref={forwardRef}>
+    <Container onClick={handleClick}  ref={ref}>
       {icon}
       <DropDown isOpen={isOpen}>
         {children}
